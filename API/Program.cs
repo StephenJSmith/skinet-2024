2ce76928ba285b5adb60373c90808c4b10d711a4
Stephen Smith <stephensmith_aus@yahoo.com.au>
2024-08-23 06:32:35 +1000

    End of section 14

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Middleware;
+using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Services;
@@ -12,43 +13,51 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(opt =>
 {
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+  opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddCors();
-builder.Services.AddSingleton<IConnectionMultiplexer>(config => 
+builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
 {
-    var connString = builder.Configuration.GetConnectionString("Redis") 
-        ?? throw new Exception("Redis connection string is null");
-    var configuration = ConfigurationOptions.Parse(connString, true);
+  var connString = builder.Configuration.GetConnectionString("Redis")
+      ?? throw new Exception("Redis connection string is null");
+  var configuration = ConfigurationOptions.Parse(connString, true);
 
-    return ConnectionMultiplexer.Connect(configuration);
+  return ConnectionMultiplexer.Connect(configuration);
 });
 builder.Services.AddSingleton<ICartService, CartService>();
+builder.Services
+    .AddIdentityApiEndpoints<AppUser>()
+    .AddEntityFrameworkStores<StoreContext>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+app.UseCors(x => 
+  x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
+app.MapGroup("api")
+  .MapIdentityApi<AppUser>();
 
 try
 {
-    using var scope = app.Services.CreateScope();
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<StoreContext>();
-    await context.Database.MigrateAsync();
-    await StoreContextSeed.SeedAsync(context);
+  using var scope = app.Services.CreateScope();
+  var services = scope.ServiceProvider;
+  var context = services.GetRequiredService<StoreContext>();
+  await context.Database.MigrateAsync();
+  await StoreContextSeed.SeedAsync(context);
 }
 catch (Exception ex)
 {
-    Console.WriteLine(ex);
-    throw;
+  Console.WriteLine(ex);
+  throw;
 }
 
 app.Run();
